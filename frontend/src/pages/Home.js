3c4b50eec528d6cff474edd9323e4228b3617dda
Rohit Handage <106050288+Rohithandage@users.noreply.github.com>
2024-11-26 22:40:23 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,17 +12,11 @@ const Home = () => {
      
  <CategoryList/>
  <BannerProduct/>
+    <VerticalCardProduct category={"Phone Covers"} heading={"Phone Covers"}/>
  <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"}/>
       <HorizontalCardProduct category={"watches"} heading={"Popular's Watches"}/>
-
+ <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"}/>
       <VerticalCardProduct category={"mobiles"} heading={"Mobiles"}/>
-      <VerticalCardProduct category={"Mouse"} heading={"Mouse"}/>
-      <VerticalCardProduct category={"televisions"} heading={"Televisions"}/>
-      <VerticalCardProduct category={"camera"} heading={"Camera & Photography"}/>
-      <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"}/>
-      <VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"}/>
-      <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"}/>
-      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"}/>
     </div>
   )
 }
